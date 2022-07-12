1b8b0d4e9b7297dddfb3c618fe366cdaa5be1c34
LuanTMT <103752171+LuanTMT@users.noreply.github.com>
2022-07-12 13:27:14 +0700

    verfiToken

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const config= require("../config/auth.config");
+
+const verfyToken = ( req, res, next ) => {
+    let token = req.headers[ "x-access-token"];
+
+    //return 403 error if token not found
+    if(!token){
+        return res.status( 403 ).json({
+            messege:" Notoken provided!",
+        });
+    }
+    
+    //verify jwt token
+    jwt.verify( token, config.secrect, ( err, decoded ) => {
+        if(err){
+            return res.status(403).json({
+                messege:"Forbiden! Requeries a token to access",
+            });
+        }
+        req.userID = decoded.id;
+        next();
+    });
+};
+module.exports = {verfyToken}
