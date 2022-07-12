573ef1d811d729a5f44257aecf78cf444760256b
Dinh Ngoc Khanh <dinhngockhanh16d2@gmail.com>
2022-07-12 19:19:37 +0700

    update

--- controller/user.controler.js
+++ controller/user.controler.js
@@ -1,17 +1,16 @@
 const db = require('../models/db.model');
-const UserModel = db.User;
-
-const getAllUser=  async (req, res) => {
+const AccountModel = db.Account;
+const getAllUser =  async (req, res) => {
     try {
       const users = await UserModel.findAll();
       if (users) {
-        res.status(200).json(users);
+        return res.status(200).json(users);
       }
       else{
-        res.status(404).json({ message: "Error: server not found data " });
+        return res.status(404).json({ message: "Error: server not found data " });
       }
     } catch (error) {
-      res.status(500).json({ message: "Server got error" });
+        returnres.status(500).json({ message: "Server got error" });
     } 
 };
 module.exports={getAllUser}
