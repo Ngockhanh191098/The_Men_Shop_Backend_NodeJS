eb56489f14f3105fb6ebbee27060f3d0861e20d3
LuanTMT <103752171+LuanTMT@users.noreply.github.com>
2022-07-21 15:55:05 +0700

    codewithCart

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const categoryRouter = require('./router/category.router');
 const productRouter = require('./router/product.router');
 const upload = require('./middlewares/upload');
 const orderRouter = require('./router/order.router');
+const cartRouter = require('./router/cart.router');
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
@@ -27,6 +28,7 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/category", categoryRouter);
 app.use("/api/v1/product", productRouter);
 app.use("/api/v1/order",orderRouter);
+app.use("/api/v1/cart",cartRouter);
 
 app.listen( PORT, () => {
     console.log(`Server is running on port ${PORT}....`);
